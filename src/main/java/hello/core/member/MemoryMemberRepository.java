4bedc6ffb3ed6b4be85e8bf2b729bcb412d977c9
minseok <minseok9128@gmail.com>
2023-04-16 20:29:13 +0900

    first commit

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // 사실은 concurrent해시맵을 써야 동시성 이슈 해결 가능
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
