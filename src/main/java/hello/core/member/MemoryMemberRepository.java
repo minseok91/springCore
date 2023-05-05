3fd270f4a55151f1c932dd6f4d1b80c99fe783fc
minseok <minseok9128@gmail.com>
2023-05-05 15:51:42 +0900

    컴포넌트 스캔
    컴포넌트 스캔 사용 방법
    - 필터(include, exclude)
    - 수동 등록 vs 자동 등록(현재 부트에서는 override false라서 같은 이름 등록시 에러남)

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>(); // 사실은 concurrent해시맵을 써야 동시성 이슈 해결 가능
